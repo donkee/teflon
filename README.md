d004d58f4ff4c0595a79a2d25a3bb727336a064a
donkeedong <chase.steenbock@gmail.com>
2022-01-10 19:14:07 -0600

    added telegram integration (not working at all)

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # teflon
+
+## installation
+
+1. install typescript globally
+
+   `npm i -g typescript`
+
+2. build and install tdLib using the build instructions generator: https://github.com/tdlib/td#building
+
+3. create a new app in the telegram system, getting an api ID and hash: https://my.telegram.org/
+
+## misc
+
+this project uses prettier to automatically format source code during commit. it also uses typescript for safe typing.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hi James');
+import path from 'node:path';
+import Client from 'tdl';
+import { TDLib } from 'tdl-tdlib-addon';
+
+const client = new Client(
+  new TDLib(path.resolve('~/td/tdlib/lib/libtdjson.so')),
+  {
+    apiId: 2222,
+    apiHash: 'hash'
+  }
+);
+
+client.on('error', console.error);
+client.on('update', (update) => {
+  console.log('Received update: ', update);
+});
+
+const main = async () => {
+  await client.connectAndLogin();
+  console.log(await client.invoke({ _: 'getMe' }));
+};
+
+main();
